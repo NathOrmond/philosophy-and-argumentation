1358ed62cf02f9b2204e236a197b7f6ca4172a63
nathan <nath.m.ormond@gmail.com>
2021-05-08 13:21:04 +0100

    save

--- src/languageproc/languageproc.js
+++ src/languageproc/languageproc.js
@@ -16,12 +16,11 @@ async function createPredicateSentences(text, subjects, forAgainst, targetNum){
                 keySentence = keySentence.split(' ');
                 console.log(keySentence)
                 // Find instance of keySentence in text
-                // for(let textIndex = 0; textIndex < text.length; textIndex++){
-                //     // Add keySentence + text keySentenceSentece to returnSubjectPredicateSentences
-                //     if(text[textIndex] == keySentence[0]){
-
-                //     }
-                // }
+                for(let textIndex = 0; textIndex < text.length; textIndex++){
+                    if(text[textIndex] == keySentence[0]){
+                        // Add keySentence + text keySentenceSentece to returnSubjectPredicateSentences
+                    }
+                }
             } else {
                 break;
             }
@@ -34,8 +33,28 @@ async function createPredicateSentences(text, subjects, forAgainst, targetNum){
     return returnSubjectPredicateSentences;
 }
 
-
-
+function isEndPoint(str, connectivesJSON){ 
+    let isEndPoint = true;
+    let falseEndings = "false-endings"
+    let exactMatches = "exact-matches"
+    falseEndings = connectivesJSON[falseEndings]
+    falseEndings.forEach((ending)=>{
+        if(str.includes(ending)){
+            isEndPoint = false;
+            break;
+        }
+    });
+    if(isEndPoint){
+        exactMatches = connectivesJSON[exactMatches]
+        exactMatches.forEach((ending)=>{
+            if(str === ending){
+                isEndPoint = false;
+                break;
+            }
+        }); 
+    }
+    return isEndPoint;
+}
 
 module.exports = {
     createPredicateSentences
